789eb1bd2ed0714d95d01c2b9aba6fdcaaf31631
Kamil Wilk <kamil.wilk@eset.com>
2025-02-02 20:43:54 +0000

    Modify landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-	import { Button, ImagePlaceholder,Footer, FooterCopyright, FooterLinkGroup, FooterLink, Tooltip, Card  } from 'flowbite-svelte'
+	import { Button, ImagePlaceholder,Footer, FooterLinkGroup, FooterLink, Tooltip, Modal, Label, Input, Textarea } from 'flowbite-svelte'
 	import { Section, ArticleAuthor, ArticleBody, ArticleHead, ArticleWrapper, BlogHead, BlogBodyWrapper } from 'flowbite-svelte-blocks';
 	import { VideoCameraSolid, ArrowRightOutline, NewspaperSolid } from 'flowbite-svelte-icons';
 	import me from '$lib/images/me.jpg'
+
+
+	let formModal = false;
 
 </script>
 <svelte:head>
@@ -12,12 +15,12 @@
 
 
 
-<div class="container mx-auto p-3 bg-white dark:text-gray-300 dark:bg-gray-800 shadow-lg rounded-lg flex flex-col md:flex-row items-center">
-	<div class="w-fulltext-center  md:w-1/3 mb-6 md:mb-0 md:mr-6">
+<div class="container mx-auto p-6 mt-5 mb-5 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300 shadow-xl rounded-2xl flex flex-col md:flex-row items-center gap-8">
+	<div class="w-full text-center  md:w-2/3 mb-6 md:mb-0 md:mr-6">
 		<img src={me} alt="Zdjęcie" class="rounded-lg shadow-lg">
 	</div>
 	<div class="w-full md:w-2/3 px-10">
-		<h1 class="mt-3 text-3xl font-bold text-center dark:text-gray-300" style="color: rgb(239,86,47,1);">Witaj! Nazywam się Karolina Kuchnia</h1>
+		<h1 class="mt-3 pb-5 text-3xl font-bold text-center dark:text-gray-300" style="color: rgb(239,86,47,1);">Witaj! Nazywam się Karolina Kuchnia</h1>
 		<h2 class="text-2xl font-bold text-green-500 dark:text-green-400 mb-4" style="color: rgb(239,86,47,1);">Kim jestem?</h2>
 		<p class="leading-relaxed">
 			Jestem pasjonatem rynku forex i tradingu, z wieloletnim doświadczeniem w tej dziedzinie. Moim celem jest pomaganie innym w osiągnięciu finansowej niezależności oraz rozwijaniu umiejętności inwestycyjnych. Pracuję z osobami, które chcą nauczyć się skutecznego handlu na rynkach finansowych i budować stabilną przyszłość.
@@ -46,7 +49,7 @@
 		</ul>
 
 		<div class="text-center mt-8">
-			<Button pill={true}>Dołącz teraz!</Button>
+			<Button pill={true} on:click={() => (formModal = true)}>Dołącz teraz!</Button>
 		</div>
 		<Section>
 			<Footer>
@@ -62,26 +65,13 @@
 				  <span class="sr-only">Twitter</span>
 				</FooterLink>
 				<Tooltip placement="bottom">Like us on Twitter</Tooltip>
-				<FooterLink href="/">
-				  <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-					<path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
-				  </svg>
-				  <span class="sr-only">Github</span>
-				</FooterLink>
-				<Tooltip placement="bottom">Star us on GitHub</Tooltip>
-				<FooterLink href="/">
-				  <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-					<path fill-rule="evenodd" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z" clip-rule="evenodd" />
-				  </svg>
-				  <span class="sr-only">Dribbble</span>
-				</FooterLink>
-				<Tooltip placement="bottom">Follow us on Dribbble</Tooltip>
 			  </FooterLinkGroup>
 			</Footer>
 		  </Section>
 	</div>
 </div>
 
+<!--
 <Section name="blog" sectionClass="relative py-6 sm:py-10 mt-5">
 	<BlogHead>
 	  <svelte:fragment slot="h2">Moj Studenci</svelte:fragment>
@@ -144,6 +134,25 @@
 	  </ArticleWrapper>
 	</BlogBodyWrapper>
   </Section>
+
+-->
+  <Modal bind:open={formModal} size="xs" autoclose={false} class="w-full">
+	<form class="flex flex-col space-y-6" action="#">
+	  <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Wyślij zapytanie</h3>
+	  <Label class="space-y-2">
+		<span>Email</span>
+		<Input type="email" name="email" placeholder="name@company.com" required />
+	  </Label>
+	  <Label class="space-y-2">
+		<span>Imię</span>
+		<Input  required />
+	  </Label>
+	  <Label class="space-y-2"><span>Twoja wiadomość</span>
+		<Textarea  placeholder="Twoja wiadomość" rows={4} name="message" />
+	  </Label>
+	  <Button type="submit" class="w-full1">Submit</Button>
+	</form>
+  </Modal>
 
 
 
